2c311abfaa36f723e96b651c70331d35c1997d48
Nguyen Viet Anh <imcpro12345@gmail.com>
2018-08-04 15:56:36 +0700

    hard vkl

--- [HW] Pong Game.py
+++ [HW] Pong Game.py
@@ -1,12 +1,10 @@
 import pygame
 from random import choice, randint
 
-height = int(input("Enter the height (<= 720): "))
-width = int(input("Enter the width:(<= 720) "))
 pygame.init()
 pygame.display.set_caption("Core game")
 
-SIZE = (width, height)
+SIZE = (640, 480)
 BG_COLOR = (52, 76, 52)
 canvas = pygame.display.set_mode(SIZE)
 
@@ -16,20 +14,25 @@ paddle_image_1 = paddle_image
 ball = pygame.image.load("assets/ball.png")
 
 x1 = 0
-y1 = randint(0, height -100)
-x2 = width - 30
-y2 = randint(0, height - 100)
+y1 = randint(0, 480 -100)
+x2 = 640 - 30
+y2 = randint(0, 480 - 100)
 
 w_pressed = False
 s_pressed = False
 
 o_pressed = False
-l_presssed = False
+l_pressed = False
 
 ball_v_x = choice([i for i in range(-5, 5) if i != 0])
 ball_v_y = 6
-ballx = width / 2
-bally = height / 2
+ballx = 320
+bally = 240
+
+# ball_v_x = 0
+# ball_v_y = 6
+# ballx = 600
+# bally = 100
 
 loop = True
 
@@ -47,7 +50,7 @@ while loop:
             elif e.key == pygame.K_o:
                 o_pressed = True
             elif e.key == pygame.K_l:
-                l_presssed = True
+                l_pressed = True
 
         elif e.type == pygame.KEYUP:
             if e.key == pygame.K_w:
@@ -57,7 +60,7 @@ while loop:
             elif e.key == pygame.K_o:
                 o_pressed = False
             elif e.key == pygame.K_l:
-                l_presssed = False
+                l_pressed = False
 
     if w_pressed:
         y1 -= 5
@@ -65,19 +68,25 @@ while loop:
         y1 += 5
     elif o_pressed:
         y2 -= 5
-    elif l_presssed:
+    elif l_pressed:
         y2 += 5
 
     ballx += ball_v_x
     bally += ball_v_y
 
-    if ballx <= x1 + 25 and bally in range(y1, y1 + 120):
+    if ballx <= x1 + 25:
+        if y1 <= bally <= y1 + 120:
+            ball_v_x = - ball_v_x
+        elif y1 - 20 <= bally <= y1 + 20 or y1 + 120 <= bally <= y1 + 125:
+            ball_v_y = - ball_v_y
+    if ballx >= x2 - 15:
+        if y2 <= bally <= y2 + 120:
+            ball_v_x = - ball_v_x
+        elif y2 - 20 <= bally <= y2 + 20 or y2 + 100 <= bally <= y2 + 125:
+            ball_v_y = - ball_v_y
+    if ballx >= 640 - 20 or ballx <= 0:
         ball_v_x = - ball_v_x
-    elif ballx >= x2 - 25 and bally in range(y2, y2 + 120):
-        ball_v_x = -ball_v_x
-    elif ballx >= width - 20 or ballx <= 0:
-        ball_v_x = - ball_v_x
-    elif bally >= height - 20 or bally <= 0:
+    elif bally >= 480 - 20 or bally <= 0:
         ball_v_y = - ball_v_y
 
     canvas.fill(BG_COLOR)
